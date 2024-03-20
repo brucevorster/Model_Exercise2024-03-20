eb48f5bc698bfaa0f54f64c9d4f1a087bd522c87
brucevorster <Brucevorster007@gmail.com>
2024-03-20 09:51:52 +0200

    Add project files.

---
+++ Week 4/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week_4.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
